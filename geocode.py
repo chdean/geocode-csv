d9b54aa652e970c9a3e0ddc879c768a374f93e50
Cameron Dean <chd@tx0.org>
2016-06-20 18:40:16 -0400

    working

--- geocode.py
+++ geocode.py
@@ -3,29 +3,28 @@
 geocode.py - batch geocode a csv file
 '''
 
-
-google_key = "" # TODO
-csv_delimiter = ";"
+google_key = ""
+csv_delimiter = ","
 
 import csv, sys, geocoder
 
 if len(sys.argv) == 4:
     in_file = sys.argv[1]
     out_file = sys.argv[2]
-    fieldnames_str = sys.argv[3]
+    location_fields_str = sys.argv[3]
 else:
     print("Usage: python geocode.py in.csv out.csv addr,city,state")
     sys.exit()
 
-fieldnames = fieldnames_str.split(',')
+location_fields = location_fields_str.split(',')
 
 def geocode(row):
     """Returns lat/long of a row"""
     attributes = []
-    for x in fieldnames:
+    for x in location_fields:
         attributes.append(row[x])
+
     full_address = ', '.join(attributes)
-    
     print("Geocoding:", full_address)
     
     g = geocoder.google(full_address, key = google_key)
@@ -41,6 +40,7 @@ with open(in_file) as csvfile:
     reader = csv.DictReader(csvfile, delimiter = csv_delimiter)
     for row in reader:
         data.append(row)
+
 for record in data:
     latlng = geocode(record)
     if latlng:
@@ -49,5 +49,13 @@ for record in data:
     else:
         record['Latitude'] = ''
         record['Longitude'] = ''
+fieldnames = list(data[0].keys())
+with open(out_file, 'x') as outcsv:
+    writer = csv.DictWriter(outcsv, 
+                            delimiter = csv_delimiter,
+                            fieldnames = fieldnames,
+                            extrasaction = 'ignore')
+    writer.writeheader()
+    for row in data:
+        writer.writerow(row)
 
-# TODO - write to new csv file
